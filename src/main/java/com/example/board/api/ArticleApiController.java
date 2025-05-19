ff60d2fa575ff714274534c71e4fcb148936fdb8
KimSeeun <9035anny@naver.com>
2025-05-19 15:36:10 +0900

    refactor : Service 계층 사용하여 Controller 리팩토링

--- src/main/java/com/example/board/api/ArticleApiController.java
+++ src/main/java/com/example/board/api/ArticleApiController.java
@@ -3,6 +3,7 @@ package com.example.board.api;
 import com.example.board.dto.ArticleForm;
 import com.example.board.entity.Article;
 import com.example.board.repository.ArticleRepository;
+import com.example.board.service.ArticleService;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,52 +21,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ArticleApiController {
     @Autowired
-    private ArticleRepository articleRepository;
+    private ArticleService articleService;
     // GET
     @GetMapping("/api/articles")
     public List<Article> index(){
-      return articleRepository.findAll();
+      return articleService.index();
     }
     @GetMapping("/api/articles/{id}")
     public Article show(@PathVariable Long id) {
-      return articleRepository.findById(id).orElse(null);
+      return articleService.show(id);
     }
     // POST
     @PostMapping("/api/articles")
-    public Article create(@RequestBody ArticleForm dto){
-        Article article = dto.toEntity();
-        return articleRepository.save(article);
+    public ResponseEntity<Article> create(@RequestBody ArticleForm dto){
+        Article created = articleService.create(dto);
+        return (created != null) ?
+            ResponseEntity.status(HttpStatus.OK).body(created) :
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
     // PATCH
     @PatchMapping("/api/articles/{id}")
     public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
-      // 1. DTO->Entity
-      Article article = dto.toEntity();
-      log.info("id: {}, article: {}", id, article.toString());
-      // 2. DB에 대상 엔티티가 있는지 조회.
-      Article target = articleRepository.findById(id).orElse(null);
-      // 3. 대상 엔티티가 없거나 수정하려는 id가 잘못됐을 경우 처리.
-      if (target == null || id != article.getId()) {
-        // 400, 잘못된 요청 응답!
-        log.info("잘못된 요청! id: {}, article:{}", id, article.toString());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-      }
-      // 4. 대상 엔티티가 있으면 수정 내용으로 업데이트하고 200 보내기.
-      target.patch(article); //target=target+article가 됨.
-      Article updated = articleRepository.save(target);
-      return ResponseEntity.status(HttpStatus.OK).body(updated);
+        Article updated = articleService.update(id, dto);
+        return (updated != null)?
+                ResponseEntity.status(HttpStatus.OK).body(updated):
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
     // DELETE
     @DeleteMapping("/api/articles/{id}")
     public ResponseEntity<Article> delete(@PathVariable Long id) {
-        //1. 대상 찾기
-        Article target = articleRepository.findById(id).orElse(null);
-        //2. 잘못된 요청 처리하기
-        if (target == null) {
-          return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        //3. 대상 삭제하기
-        articleRepository.delete(target);
-        return ResponseEntity.status(HttpStatus.OK).build();
+        Article deleted = articleService.delete(id);
+        return (deleted != null) ?
+                ResponseEntity.status(HttpStatus.NO_CONTENT).build():
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 }
