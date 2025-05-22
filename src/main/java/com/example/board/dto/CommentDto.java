9500c6be10f63a924779ae5e270b3af1c88980b1
KimSeeun <9035anny@naver.com>
2025-05-22 11:19:50 +0900

    refactor: articleId의 @JsonProperty 주석처리

--- src/main/java/com/example/board/dto/CommentDto.java
+++ src/main/java/com/example/board/dto/CommentDto.java
@@ -14,7 +14,7 @@ import lombok.ToString;
 
 public class CommentDto {
     private Long id;
-    @JsonProperty("article_id")
+//    @JsonProperty("article_id")
     private Long articleId;
     private String nickname;
     private String body;
